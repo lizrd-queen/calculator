38fe83dae994c9dffb509e40bea4ff45d7d2dc73
lizrd-queen <lizrd.roberts96@gmail.com>
2023-11-25 21:32:14 -0600

    add function to set operator

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let num1 = 0;
+let num2 = 0
+let operator = '';
+
+function setOperator(selectedOperator) {
+    operator = selectedOperator;
+    num1 = parseFloat(currentInput);
+    currentInput = '';
+}
+
+
 function add (a, b){
     return (a+b);
 }
